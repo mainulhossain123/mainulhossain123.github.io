daf84735da43da688c60e14148d45fff1deef343
Mainul Hossain <mainul.h.a@hotmail.co.uk>
2025-11-26 20:47:41 +0600

    Updating skills and experience

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -23,10 +23,16 @@ module ExternalPosts
     end
 
     def fetch_from_rss(site, src)
-      xml = HTTParty.get(src['rss_url']).body
-      return if xml.nil?
-      feed = Feedjira.parse(xml)
-      process_entries(site, src, feed.entries)
+      begin
+        xml = HTTParty.get(src['rss_url']).body
+        return if xml.nil? || xml.empty?
+        feed = Feedjira.parse(xml)
+        process_entries(site, src, feed.entries)
+      rescue Feedjira::NoParserAvailable => e
+        puts "Warning: Could not parse RSS feed from #{src['name']}: #{e.message}"
+      rescue StandardError => e
+        puts "Error fetching RSS from #{src['name']}: #{e.message}"
+      end
     end
 
     def process_entries(site, src, entries)
@@ -68,10 +74,14 @@ module ExternalPosts
 
     def fetch_from_urls(site, src)
       src['posts'].each do |post|
-        puts "...fetching #{post['url']}"
-        content = fetch_content_from_url(post['url'])
-        content[:published] = parse_published_date(post['published_date'])
-        create_document(site, src['name'], post['url'], content)
+        begin
+          puts "...fetching #{post['url']}"
+          content = fetch_content_from_url(post['url'])
+          content[:published] = parse_published_date(post['published_date'])
+          create_document(site, src['name'], post['url'], content)
+        rescue StandardError => e
+          puts "Error fetching post from #{post['url']}: #{e.message}"
+        end
       end
     end
 
