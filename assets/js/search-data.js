7311d31a20a426c79120a5078c2b8751951991b5
mainulhossain123 <71011546+mainulhossain123@users.noreply.github.com>
2025-12-29 09:04:00 +0000

    Deploying to gh-pages from @ mainulhossain123/mainulhossain123.github.io@a365d2e9141675e2dc6e38d2e20b81f2ec46149b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -366,7 +366,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-infrastructure-self-healing-agent",
+          title: 'Infrastructure Self-Healing Agent',
+          description: "Auto-remediation bot that detects application failures, triggers automated recovery, and sends real-time incident notifications",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_infra_autofix_agent/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
